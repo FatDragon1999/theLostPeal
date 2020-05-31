a289f3120837cdaf4c21950217aae1ea45bcb575
wjh776a68 <wjh776a68@outlook.com>
2020-06-01 00:27:10 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+jQuery(document).ready(function($) {
+
+			if ($.fn.fsvs) {
+				var slider = $.fn.fsvs({
+					speed: 1000,
+					nthClasses: 4,
+					mouseDragEvents: false
+				});
+			}
+
+			if ($.fn.flare) {
+				var flares = $('.flare').flare();
+				for (var flare in flares) {
+					//flares[flare].reset();
+				}
+			}
+
+			var sectionHeight = $('#fsvs-body > .slide:eq(0)').height();
+			$('#fsvs-body > .slide').each(function() {
+				var section = $(this),
+					item = $('.item', section),
+					demo = $('.demo', section),
+					itemHeight = item.outerHeight(),
+					demoHeight = demo.outerHeight();
+				item.css({
+					marginTop: ((sectionHeight - itemHeight) / 2) + 'px'
+				});
+				demo.css({
+					marginTop: ((sectionHeight - demoHeight) / 2) + 'px'
+				});
+
+			});
+
+			$(window).load(function() {
+				
+			});
+			$(function() {
+				$('.menu1').click(function() {
+					if ($(".menu1").next().is(':hidden')) {
+						$(".menu1").next().show();
+						$(".menu2").next().hide();
+						$(".menu3").next().hide();
+						$(".menu1").attr("src", 'img/close.png');
+					} else {
+						$(".menu1").next().hide();
+						$(".menu1").attr("src", 'img/menu.png');
+					}
+				})
+				$('.menu2').click(function() {
+					if ($(".menu2").next().is(':hidden')) {
+						$(".menu2").next().show();
+						$(".menu1").next().hide();
+						$(".menu3").next().hide();
+						$(".menu2").attr("src", 'img/close.png');
+					} else {
+						$(".menu2").next().hide();
+						$(".menu2").attr("src", 'img/menu.png');
+					}
+				})
+				$('.menu3').click(function() {
+					if ($(".menu3").next().is(':hidden')) {
+						$(".menu3").next().show();
+						$(".menu2").next().hide();
+						$(".menu1").next().hide();
+						$(".menu3").attr("src", 'img/close.png');
+					} else {
+						$(".menu3").next().hide();
+						$(".menu3").attr("src", 'img/menu.png');
+					}
+				})
+			})
+});
+
+
---
+++ assets/js/original.js
@@ -0,0 +1,87 @@
+
+ var imgSrcArr = [
+        'img',
+
+    ];
+
+    var imgWrap = [];
+
+    function preloadImg(arr) {
+        for(var i =0; i< arr.length ;i++) {
+            imgWrap[i] = new Image();
+            imgWrap[i].src = arr[i];
+        }
+    }
+
+    preloadImg(imgSrcArr);
+
+
+
+function startsearch(){
+			
+	var incomeinform = document.getElementById("inputmymenu").value;
+	switch(incomeinform){
+		case "":case null:case " ":break;
+		case "easteregg": alert("here");break;
+		default:
+			window.location.href="https://baike.baidu.com/item/" + incomeinform;
+	}
+}
+	
+		
+function navtomain(){
+	window.location.href="main.html";
+}
+
+function func(id){						
+	document.getElementById(id).style.display="block";
+	//document.getElementById(id).style.transform="scaleX(1.05)";
+	//document.getElementById(id).style.transform="scale(1.05)";		
+}
+function func1(id){
+	document.getElementById(id).style.display="none";
+	//document.getElementById(id).style.transform="scaleX(1.0)";
+	//document.getElementById(id).style.transform="scale(1.0) ";				
+}
+
+//document.getElementById("banner").height = 0.7 * ((document.documentElement.scrollHeight >document.documentElement.clientHeight) ? document.documentElement.scrollHeight : document.documentElement.clientHeight).toString();
+//document.getElementById("banner").height = window.getComputedStyle("cirtizen").getPropertyValue('height');
+
+function changedisplay(id,coverid,lastid,input,linkto){		
+	var currentitem = document.getElementById(id);
+	document.getElementById(lastid).background=currentitem.src;
+	
+	document.getElementById("navid1").style.display="none";
+	document.getElementById("navid2").style.display="none";
+	document.getElementById("navid3").style.display="none";
+	document.getElementById("menu1id").src="img/menu.png";
+	document.getElementById("menu2id").src="img/menu.png";
+	document.getElementById("menu3id").src="img/menu.png";
+	
+	currentitem.src=input;		
+	
+	//element = document.getElementById(id);
+	//element.classList.remove("mydefault");
+	//element.offsetWidth = element.offsetWidth;
+	//element.classList.add("myfirst");
+	
+	var elm = document.getElementById(id);
+	newone = elm.cloneNode(true);
+	elm.parentNode.replaceChild(newone, elm);
+	
+	
+	currentitem.onclick=function(){
+		//alert("here");
+		window.location.href=linkto;
+		//console.log(linkto);
+		
+	}
+	//console.log(currentitem.onclick);
+	
+	document.getElementById(coverid).onclick=function(){
+		//alert("here2");
+		window.location.href=linkto;
+	}
+	//console.log(document.getElementById(coverid).onclick);
+	//currentitem.style.animation="myfirst";
+}
